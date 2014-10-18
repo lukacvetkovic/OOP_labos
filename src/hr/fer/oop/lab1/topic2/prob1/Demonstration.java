b7b6b55f12fd233e14cddad3c4546801a811877d
Luka Cvetković <l.cvetk@gmail.com>
2014-10-18 19:33:48 +0200

    Promjene male

--- src/hr/fer/oop/lab1/topic2/prob1/Demonstration.java
+++ src/hr/fer/oop/lab1/topic2/prob1/Demonstration.java
@@ -6,8 +6,12 @@ import hr.fer.oop.lab1.topic2.pic.PictureDisplay;
 public class Demonstration {
 
 	public static void main(String[] args) {
-		Picture slika= new Picture(50, 100);
+		Picture slika= new Picture(500, 1000);
+		Point jedan=new Point(10,40);
+		Point dva=new Point(40,10);
+		Rectangle rectangle=new Rectangle(10,10, jedan, dva);
 		PictureDisplay display= new PictureDisplay();
+		rectangle.drawOnPicture(slika);
 		display.showPicture(slika);
 	}
 
--- src/hr/fer/oop/lab1/topic2/prob1/Rectangle.java
+++ src/hr/fer/oop/lab1/topic2/prob1/Rectangle.java
@@ -5,18 +5,40 @@ import hr.fer.oop.lab1.topic2.pic.Picture;
 public class Rectangle {
 	
 	public double width,height;
-	Point x;
-	Point y;
-
-	public Rectangle(double width, double height) {
+	Point gornjiLijevi;
+	Point gornjiDesni;
+	
+	
+	
+	
+	public Rectangle(double width, double height, Point gornjiLijevi,
+			Point gornjiDesni) {
 		super();
 		this.width = width;
 		this.height = height;
+		this.gornjiLijevi = gornjiLijevi;
+		this.gornjiDesni = gornjiDesni;
+	}
+	
+	public Rectangle(Rectangle rectangle) {
+		super();
+		this.width = rectangle.width;
+		this.height = rectangle.height;
+		this.gornjiLijevi = rectangle.gornjiLijevi;
+		this.gornjiDesni = rectangle.gornjiDesni;
 		
 	}
 	
+	
+
 	public void drawOnPicture(Picture slika){
-		//ne znam kak to	
+		slika.turnPixelOn(gornjiLijevi.x, gornjiDesni.y);
+		Point pom=new Point(gornjiLijevi.x,(int) (gornjiLijevi.y+height));
+		slika.turnPixelOn(pom.x,pom.y);
+		
+		slika.turnPixelOn(gornjiDesni.x,gornjiDesni.y );
+		Point novi=new Point(gornjiDesni.x,(int) (gornjiDesni.y+height));
+		slika.turnPixelOn(novi.x,(int) (novi.y+height*2) );
 		
 	}
 
