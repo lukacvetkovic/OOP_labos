a48a6b792140560c019b3b7e95e70b9ab575871f
lukacvetkovic <l.cvetk@gmail.com>
2014-12-03 23:43:40 +0100

    radi sve osim cd

--- Lab3/src/hr/fer/oop/lab3/topic1/shell/HelpCommand.java
+++ Lab3/src/hr/fer/oop/lab3/topic1/shell/HelpCommand.java
@@ -1,5 +1,7 @@
 package hr.fer.oop.lab3.topic1.shell;
 
+import hr.fer.oop.lab3.topic1.collections.SimpleHashtable.TableEntry;
+
 public class HelpCommand extends AbstractCommand{
 
 	public HelpCommand() {
@@ -9,14 +11,11 @@ public class HelpCommand extends AbstractCommand{
 
 	@Override
 	public CommandStatus execute(Environment e, String s) {
-		AbstractCommand[] listOfCommands= new AbstractCommand[4];
-		listOfCommands[0]= new HelpCommand();
-		listOfCommands[1]= new CdCommand();
-		listOfCommands[2]= new QuitCommand();
-		listOfCommands[3]= new TerminalCommand();
 		
-		for(AbstractCommand ab : listOfCommands){
-			System.out.println(ab.getCommandName()+"\t"+ab.getCommandDescription());
+		for(Object command:e.commands()){
+			TableEntry entery= (TableEntry)command;
+			ShellCommand com= (ShellCommand)entery.getValue();
+			System.out.println(com.getCommandName()+"\t"+com.getCommandDescription());
 		}
 		return CommandStatus.CONTINUE;
 	}
