3b542e5c41f05781b424cbf14d9de19d0e109b9f
lukacvetkovic <l.cvetk@gmail.com>
2014-12-04 00:31:12 +0100

    Radi ali ne gleda da li taj path postoji uopce

--- Lab3/src/hr/fer/oop/lab3/topic1/shell/CdCommand.java
+++ Lab3/src/hr/fer/oop/lab3/topic1/shell/CdCommand.java
@@ -1,5 +1,10 @@
 package hr.fer.oop.lab3.topic1.shell;
 
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+import com.sun.org.apache.regexp.internal.recompile;
+
 public class CdCommand extends AbstractCommand {
 
 	public CdCommand() {
@@ -8,8 +13,27 @@ public class CdCommand extends AbstractCommand {
 
 	@Override
 	public CommandStatus execute(Environment e, String s) {
-		// TODO Auto-generated method stub
-		return null;
+		String pathString = s.split(" ")[0];
+		if (pathString.equals("..")) {
+			Path path = e.getActiveTerminal().getCurrentPath();
+			path = path.toAbsolutePath().normalize().getParent();
+			if (path.toAbsolutePath().normalize().getParent() == null) {
+				System.out.println("Error");
+				return CommandStatus.CONTINUE;
+			} else {
+				e.getActiveTerminal().setCurrentPath(path);
+				System.out.println("Current directory id now set to: "
+						+ path.toAbsolutePath().normalize().toString());
+			}
+		} else {
+			Path path= Paths.get(pathString);
+			e.getActiveTerminal().setCurrentPath(path);
+			System.out.println("Current directory id now set to: "
+					+ path.toAbsolutePath().normalize().toString());
+
+		}
+
+		return CommandStatus.CONTINUE;
 	}
 
 }
